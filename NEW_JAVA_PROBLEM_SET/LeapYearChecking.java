3f5edc0e86f208327b4226d14eeb6540db5f6745
Kundu-A <kunduarpan43@gmail.com>
2025-01-01 18:34:33 +0530

    Leap year checking is modified - added new two methods - Extract last two digits of the given year input , and Convert days into month , weeks and days

--- NEW_JAVA_PROBLEM_SET/LeapYearChecking.java
+++ NEW_JAVA_PROBLEM_SET/LeapYearChecking.java
@@ -6,6 +6,22 @@ class Solution{
         else
             return false;
     }
+    public static int extractLastTwoDigit(int year){
+        String temp=String.valueOf(year);
+        String result="";
+        result=result+temp.charAt(temp.length()-2)+temp.charAt(temp.length()-1);
+        return Integer.parseInt(result);
+    }
+    public static void convertDays(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Please enter Days into Year , Month and Days: ");
+        int day=sc.nextInt();
+        int year=day/365;
+        int weeks=(day-(year*365))/7;
+        int days=(day-(year*365))%7;
+        System.out.println(day+" days = "+"\nYear = "+year+" ,Weeks = "+weeks+" ,Days = "+days);
+        sc.close();
+    }
 }
 public class LeapYearChecking {
     public static void main(String[] args) {
@@ -18,6 +34,8 @@ public class LeapYearChecking {
             System.out.println(year+" is Leap Year.");
         else
             System.out.println(year+" is not Leap Year.");
+        System.out.println("Last two digits = "+Solution.extractLastTwoDigit(year));
+        Solution.convertDays();
         sc.close();
     }
 }
