c2332d018eb60628d3fa803e991fad81b7eb90da
Kundu-A <kunduarpan43@gmail.com>
2025-01-25 23:57:36 +0530

    Find out total number of digits in a number

---
+++ NEW_JAVA_PROBLEM_SET/Interview_CountNumbers.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Interview_CountNumbers {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Please enter a number: ");
+        long num=sc.nextLong();
+        System.out.println(String.valueOf(num).length());
+        sc.close();
+    }
+}
