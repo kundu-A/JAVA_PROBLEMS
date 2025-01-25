2d399e8c872636b9be2cd1c2f6b7a0bc3bc11ae3
Kundu-A <kunduarpan43@gmail.com>
2025-01-25 12:54:36 +0530

    Sort a given array

---
+++ NEW_JAVA_PROBLEM_SET/Interview_SortArray.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Interview_SortArray {
+    public static int[] sortArray(int[] ar){
+        for(int i=0;i<ar.length;i++){
+            for(int j=i+1;j<ar.length;j++){
+                if(ar[i]>ar[j]){
+                    int temp=ar[i];
+                    ar[i]=ar[j];
+                    ar[j]=temp;
+                }
+            }
+        }
+        return ar;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Please enter the size of the array: ");
+        int size=sc.nextInt();
+        int ar[]=new int[size];
+        System.out.println("Please enter values in the array: ");
+        for(int i=0;i<size;i++)
+            ar[i]=sc.nextInt();
+        sortArray(ar);
+        for (int i : ar) {
+            System.out.print(i+" ");
+        }
+        sc.close();
+    }
+}
