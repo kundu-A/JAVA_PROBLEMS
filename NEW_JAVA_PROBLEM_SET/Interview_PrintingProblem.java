e02f6904f4ed653d5836d54356d748fa7b14740c
Kundu-A <kunduarpan43@gmail.com>
2025-01-29 19:25:16 +0530

    Interview Problem - aabbccdd -> a2b2c2d2

---
+++ NEW_JAVA_PROBLEM_SET/Interview_PrintingProblem.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Interview_PrintingProblem {
+    public static void main(String[] args) {
+        System.out.println("Intput: aabbccdd");
+        System.out.println("Output: a2b2c2d2");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Please enter a String: ");
+        String str=sc.next();
+        List<Character> uniCharacters=new ArrayList<>();
+        for(int i=0;i<str.length();i++){
+            if(uniCharacters.indexOf(str.charAt(i))==-1)
+                uniCharacters.add(str.charAt(i));
+        }
+        String s="";
+        for(int i=0;i<uniCharacters.size();i++){
+            int count=0;
+            for(int j=0;j<str.length();j++){
+                if(uniCharacters.get(i)==str.charAt(j))
+                    count++;
+            }
+            s=s+uniCharacters.get(i)+count;
+        }
+        System.out.println(s);
+        sc.close();
+    }
+}
