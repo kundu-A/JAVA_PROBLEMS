3dcca80ea93738051aca8974221d756e32443aaf
Kundu-A <kunduarpan43@gmail.com>
2025-10-10 20:55:59 +0530

    Count vowel and consonant of a given string

---
+++ SDE_DSA_QUESTIONS/CountVowelAndConsonant.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class CountVowelAndConsonant {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Please enter a string: ");
+        String str=sc.nextLine();
+        count(str);
+        sc.close();
+    }
+    public static void count(String str){
+        str.toLowerCase();
+        int vowelCount=0,consonantCount=0;
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)=='a' || str.charAt(i)=='e' || str.charAt(i)=='i' 
+            || str.charAt(i)=='o' || str.charAt(i)=='u')
+                vowelCount++;
+            else{
+                if(str.charAt(i)!=' ')
+                    consonantCount++;
+            }
+        }
+        System.out.println("Tota no. of Vowel: "+vowelCount+
+                            "\nTotal no. of Consonant: "+consonantCount);
+    }
+}
